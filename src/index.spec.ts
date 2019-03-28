f61748f3893c233fc2bc7b063cf3ec225478819f
Bergur Hallgrímsson <bergur@bergur.is>
2019-03-28 13:05:22 +0000

    Formatting and lint rules to conform to TM internal standard

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,118 +1,114 @@
-import { expect } from "chai";
-import * as kennitala from "./";
+import { expect } from 'chai'
+import * as kennitala from './'
 
-describe("kennitala", () => {
-  it("should be able to check if kennitala is of a company", () => {
-    expect(kennitala.isCompany("5811131290")).to.equal(true);
+describe('kennitala', () => {
+  it('should be able to check if kennitala is of a company', () => {
+    expect(kennitala.isCompany('5811131290')).to.equal(true)
     expect(kennitala.isCompany("150484-2359drop table('secretTable')")).to.eq(
-      false,
-    );
-    expect(() => kennitala.isCompany("1234567890")).to.throw(
-      "Invalid kennitala: 1234567890",
-    );
-  });
-  it("isLegalKt should check if a social security number is valid", () => {
-    expect(kennitala.isValid("1504842359")).to.equal(true);
-    expect(kennitala.isValid(1504842359)).to.equal(true);
-  });
-  it("formatAndValidate", () => {
-    expect(kennitala.cleanAndValidate("301794989")).to.equal("0301794989");
-    expect(() => kennitala.cleanAndValidate("adfasdf")).to.throw(
-      "Invalid kennitala: adfasdf",
-    );
+      false
+    )
+    expect(() => kennitala.isCompany('1234567890')).to.throw(
+      'Invalid kennitala: 1234567890'
+    )
+  })
+  it('isLegalKt should check if a social security number is valid', () => {
+    expect(kennitala.isValid('1504842359')).to.equal(true)
+    expect(kennitala.isValid(1504842359)).to.equal(true)
+  })
+  it('formatAndValidate', () => {
+    expect(kennitala.cleanAndValidate('301794989')).to.equal('0301794989')
+    expect(() => kennitala.cleanAndValidate('adfasdf')).to.throw(
+      'Invalid kennitala: adfasdf'
+    )
     expect(() => kennitala.cleanAndValidate()).to.throw(
-      "Invalid kennitala: undefined",
-    );
-    expect(kennitala.cleanAndValidate("150484-2359")).to.equal("1504842359");
-    expect(kennitala.cleanAndValidate("30179-4989")).to.equal("0301794989");
-    expect(() => kennitala.cleanAndValidate("2203710000")).to.throw(
-      "Invalid kennitala: 2203710000",
-    );
-  });
-  it("should clean kennitala", () => {
+      'Invalid kennitala: undefined'
+    )
+    expect(kennitala.cleanAndValidate('150484-2359')).to.equal('1504842359')
+    expect(kennitala.cleanAndValidate('30179-4989')).to.equal('0301794989')
+    expect(() => kennitala.cleanAndValidate('2203710000')).to.throw(
+      'Invalid kennitala: 2203710000'
+    )
+  })
+  it('should clean kennitala', () => {
     expect(
-      kennitala.clean("adklæj1015- 9$df123drop table('importantone')4"),
-    ).to.equal("0101591234");
-    expect(kennitala.clean(1234567890)).to.equal("1234567890");
-  });
-  it("should add dash to kennitala", () => {
-    expect(kennitala.format("1504842359")).to.equal("150484-2359");
-    expect(kennitala.format("101842359")).to.equal("010184-2359");
-  });
-  describe("is kenntala part", () => {
-    it("should return false for empty string or undefined", () => {
-      expect(kennitala.isKennitalaPart("")).to.equal(false);
-      expect(kennitala.isKennitalaPart()).to.equal(false);
-    });
-    it("should return true for numbers", () => {
-      expect(kennitala.isKennitalaPart("123")).to.equal(true);
-    });
+      kennitala.clean("adklæj1015- 9$df123drop table('importantone')4")
+    ).to.equal('0101591234')
+    expect(kennitala.clean(1234567890)).to.equal('1234567890')
+  })
+  it('should add dash to kennitala', () => {
+    expect(kennitala.format('1504842359')).to.equal('150484-2359')
+    expect(kennitala.format('101842359')).to.equal('010184-2359')
+  })
+  describe('is kenntala part', () => {
+    it('should return false for empty string or undefined', () => {
+      expect(kennitala.isKennitalaPart('')).to.equal(false)
+      expect(kennitala.isKennitalaPart()).to.equal(false)
+    })
+    it('should return true for numbers', () => {
+      expect(kennitala.isKennitalaPart('123')).to.equal(true)
+    })
 
-    it("should return true for numbers with dash or space", () => {
-      expect(kennitala.isKennitalaPart("123-21")).to.equal(true);
-      expect(kennitala.isKennitalaPart("123 21 ")).to.equal(true);
-    });
+    it('should return true for numbers with dash or space', () => {
+      expect(kennitala.isKennitalaPart('123-21')).to.equal(true)
+      expect(kennitala.isKennitalaPart('123 21 ')).to.equal(true)
+    })
 
-    it("should return false for none numbers", () => {
-      expect(kennitala.isKennitalaPart("asd 12")).to.equal(false);
-    });
-  });
-  describe("what age is the owner of kennitala", () => {
-    it("should return the age for kennitala", () => {
-      const refDate = new Date(2015, 2, 1);
+    it('should return false for none numbers', () => {
+      expect(kennitala.isKennitalaPart('asd 12')).to.equal(false)
+    })
+  })
+  describe('what age is the owner of kennitala', () => {
+    it('should return the age for kennitala', () => {
+      const refDate = new Date(2015, 2, 1)
       expect(
-        kennitala.getAge("2903992389", new Date(2017, 2, 28, 7, 22, 1)),
-      ).to.eq(17);
+        kennitala.getAge('2903992389', new Date(2017, 2, 28, 7, 22, 1))
+      ).to.eq(17)
       expect(
-        kennitala.getAge("2903992389", new Date(2017, 2, 29, 7, 22, 1)),
-      ).to.eq(18);
-      expect(kennitala.getAge("1504842359", new Date(2015, 3, 14))).to.equal(
-        30,
-      );
-      expect(kennitala.getAge("1504842359", new Date(2015, 3, 15))).to.equal(
-        31,
-      );
-      expect(kennitala.getAge("0408823919", refDate)).to.equal(32);
-      expect(kennitala.getAge("0709042840", refDate)).to.equal(10);
-      expect(kennitala.getAge("0102713129", new Date(2017, 1, 1))).to.equal(46);
-      expect(kennitala.getAge("2203710000", new Date(2017, 11, 15))).to.equal(
-        46,
-      );
-    });
-    it("should return age of company kennitala", () => {
-      const refDate = new Date(2015, 3, 1);
-      expect(kennitala.getAge("5004850519", refDate)).to.equal(29);
-      expect(kennitala.getAge("5505071960", refDate)).to.equal(7);
-    });
-    it("should return age of child younger than 1", () => {
-      const refDate = new Date(1984, 6, 6);
-      expect(kennitala.getAge("1504842359", refDate)).to.equal(82 / 365.2422);
-      expect(kennitala.getAge("3112832359", refDate)).to.equal(188 / 365.2422);
-    });
-  });
-  describe("dates", () => {
+        kennitala.getAge('2903992389', new Date(2017, 2, 29, 7, 22, 1))
+      ).to.eq(18)
+      expect(kennitala.getAge('1504842359', new Date(2015, 3, 14))).to.equal(30)
+      expect(kennitala.getAge('1504842359', new Date(2015, 3, 15))).to.equal(31)
+      expect(kennitala.getAge('0408823919', refDate)).to.equal(32)
+      expect(kennitala.getAge('0709042840', refDate)).to.equal(10)
+      expect(kennitala.getAge('0102713129', new Date(2017, 1, 1))).to.equal(46)
+      expect(kennitala.getAge('2203710000', new Date(2017, 11, 15))).to.equal(
+        46
+      )
+    })
+    it('should return age of company kennitala', () => {
+      const refDate = new Date(2015, 3, 1)
+      expect(kennitala.getAge('5004850519', refDate)).to.equal(29)
+      expect(kennitala.getAge('5505071960', refDate)).to.equal(7)
+    })
+    it('should return age of child younger than 1', () => {
+      const refDate = new Date(1984, 6, 6)
+      expect(kennitala.getAge('1504842359', refDate)).to.equal(82 / 365.2422)
+      expect(kennitala.getAge('3112832359', refDate)).to.equal(188 / 365.2422)
+    })
+  })
+  describe('dates', () => {
     // Month is zero based
-    [
-      { date: new Date(1984, 3, 15), kt: "1504842009" },
-      { date: new Date(1983, 4, 6), kt: "0605832189" },
-      { date: new Date(1936, 0, 8), kt: "0801362189" },
-      { date: new Date(1972, 11, 31), kt: "3112722099" },
+    ;[
+      { date: new Date(1984, 3, 15), kt: '1504842009' },
+      { date: new Date(1983, 4, 6), kt: '0605832189' },
+      { date: new Date(1936, 0, 8), kt: '0801362189' },
+      { date: new Date(1972, 11, 31), kt: '3112722099' }
     ].forEach((data: { date: Date; kt: string }) => {
-      it("should make new valid kennitala", () => {
-        const kt = kennitala.makeKennitala(data.date);
-        expect(kennitala.isValid(kt)).to.eq(true);
-        expect(kt).to.eq(data.kt);
-      });
-    });
-  });
-  it("should verify if kennitala has a valid birthdate", () => {
-    expect(kennitala.isValidDate("3106162189")).to.eq(false);
-    expect(kennitala.isValidDate("1504842359")).to.eq(true);
-    expect(kennitala.isValidDate("2902121239")).to.eq(true);
-    expect(kennitala.isValidDate("2902131239")).to.eq(false);
-  });
-  it("kennitala with invalid month should not be valid", () => {
-    expect(kennitala.isValid("590881659")).to.eq(false);
-    expect(kennitala.isValid("0590881659")).to.eq(false);
-  });
-});
+      it('should make new valid kennitala', () => {
+        const kt = kennitala.makeKennitala(data.date)
+        expect(kennitala.isValid(kt)).to.eq(true)
+        expect(kt).to.eq(data.kt)
+      })
+    })
+  })
+  it('should verify if kennitala has a valid birthdate', () => {
+    expect(kennitala.isValidDate('3106162189')).to.eq(false)
+    expect(kennitala.isValidDate('1504842359')).to.eq(true)
+    expect(kennitala.isValidDate('2902121239')).to.eq(true)
+    expect(kennitala.isValidDate('2902131239')).to.eq(false)
+  })
+  it('kennitala with invalid month should not be valid', () => {
+    expect(kennitala.isValid('590881659')).to.eq(false)
+    expect(kennitala.isValid('0590881659')).to.eq(false)
+  })
+})
--- src/index.ts
+++ src/index.ts
@@ -11,18 +11,18 @@ const isMod11 = (kt: string): boolean => {
       4 * Number(kt.charAt(5)) +
       3 * Number(kt.charAt(6)) +
       2 * Number(kt.charAt(7))) %
-      11);
-  if (mod11 === 11 && kt.charAt(8) === "0") {
-    return true;
+      11)
+  if (mod11 === 11 && kt.charAt(8) === '0') {
+    return true
   }
   if (mod11 !== Number(kt.charAt(8))) {
-    return false;
+    return false
   }
-  return true;
-};
+  return true
+}
 export function format(kt: string): string {
-  kt = clean(kt);
-  return kt.substr(0, 6) + "-" + kt.substr(6, 10);
+  kt = clean(kt)
+  return kt.substr(0, 6) + '-' + kt.substr(6, 10)
 }
 export function makeKennitala(birthdate: Date): string {
   const digits = [
@@ -33,8 +33,8 @@ export function makeKennitala(birthdate: Date): string {
     Math.floor((birthdate.getFullYear() % 100) / 10),
     birthdate.getFullYear() % 10,
     2,
-    0,
-  ];
+    0
+  ]
   const vartalafunc = (d: number[]) => {
     return (
       11 -
@@ -47,118 +47,118 @@ export function makeKennitala(birthdate: Date): string {
         d[6] * 3 +
         d[7] * 2) %
         11)
-    );
-  };
-  const vartala = vartalafunc(digits);
-  if (vartala === 11) {
-    digits.push(0);
-  } else if (vartala === 10) {
-    digits[7] = digits[7] + 1;
-    digits.push(vartalafunc(digits));
-  } else {
-    digits.push(vartala);
+    )
   }
-  digits.push(Math.floor((birthdate.getFullYear() / 100) % 10));
-  return digits.join("");
+  const vartala = vartalafunc(digits)
+  if (vartala === 11) {
+    digits.push(0)
+  } else if (vartala === 10) {
+    digits[7] = digits[7] + 1
+    digits.push(vartalafunc(digits))
+  } else {
+    digits.push(vartala)
+  }
+  digits.push(Math.floor((birthdate.getFullYear() / 100) % 10))
+  return digits.join('')
 }
 export function clean(kt?: string | number): string {
-  kt = kt || "";
-  if (typeof kt === "number") {
-    kt = kt.toString();
+  kt = kt || ''
+  if (typeof kt === 'number') {
+    kt = kt.toString()
   }
   if (kt) {
-    kt = kt.replace(/(\D)+/g, "");
+    kt = kt.replace(/(\D)+/g, '')
   }
   if (kt.length === 9) {
-    kt = "0" + kt; // Stutt kennitala (eða ólögleg), bætum einu núlli fyrir framan.
+    kt = '0' + kt // Stutt kennitala (eða ólögleg), bætum einu núlli fyrir framan.
   }
-  return kt;
+  return kt
 }
 export function isValid(kt: string | number): boolean {
-  kt = clean(kt);
+  kt = clean(kt)
   if (
-    typeof kt !== "string" ||
+    typeof kt !== 'string' ||
     kt.length < 9 ||
     !kt.match(/^[\d\.]/) ||
-    kt === "0000000000" ||
+    kt === '0000000000' ||
     Number(kt.substr(kt.length - 8, 2)) > 13 ||
     Number(kt.substr(kt.length - 8, 2)) === 0
   ) {
-    return false;
+    return false
   }
-  return isMod11(kt);
+  return isMod11(kt)
 }
 export function cleanAndValidate(kt?: string | number): string {
-  const cleanKt = clean(kt);
+  const cleanKt = clean(kt)
   if (!isValid(cleanKt)) {
-    throw new Error("Invalid kennitala: " + kt);
+    throw new Error('Invalid kennitala: ' + kt)
   }
-  return cleanKt;
+  return cleanKt
 }
 export function getCleanIfValid(kt?: string | number): string {
-  kt = clean(kt);
+  kt = clean(kt)
   if (!isValid(kt)) {
-    return "";
+    return ''
   }
-  return kt;
+  return kt
 }
 export function isValidDate(kt: string | number): boolean {
-  const stringKt = clean(kt);
-  let day = Number(stringKt.substring(0, 2));
+  const stringKt = clean(kt)
+  let day = Number(stringKt.substring(0, 2))
   // if it is company
   if (day > 31) {
-    day = day - 40;
+    day = day - 40
   }
-  const month = Number(stringKt.substring(2, 4));
+  const month = Number(stringKt.substring(2, 4))
   // Adding the century year
   const year =
     (Number(stringKt.substring(9, 10)) === 0 ? 2000 : 1900) +
-    Number(stringKt.substring(4, 6));
-  const date = new Date(year, month - 1, day);
+    Number(stringKt.substring(4, 6))
+  const date = new Date(year, month - 1, day)
   if (
-    Object.prototype.toString.call(date) === "[object Date]" &&
+    Object.prototype.toString.call(date) === '[object Date]' &&
     date.getMonth() === month - 1
   ) {
-    return true;
+    return true
   }
-  return false;
+  return false
 }
 
 export function getBirthdate(kt: string | number): Date {
-  const stringKt = clean(kt);
-  let day = Number(stringKt.substring(0, 2));
+  const stringKt = clean(kt)
+  let day = Number(stringKt.substring(0, 2))
   // if it is company
   if (day > 31) {
-    day = day - 40;
+    day = day - 40
   }
-  const month = Number(stringKt.substring(2, 4));
+  const month = Number(stringKt.substring(2, 4))
   // Adding the century year
   const year =
     (Number(stringKt.substring(9, 10)) === 0 ? 2000 : 1900) +
-    Number(stringKt.substring(4, 6));
+    Number(stringKt.substring(4, 6))
   if (!isValidDate(stringKt)) {
-    throw new Error("Invalid date of birth. (kennitala: " + kt + ")");
+    throw new Error('Invalid date of birth. (kennitala: ' + kt + ')')
   }
-  return new Date(year, Number(month) - 1, day);
+  return new Date(year, Number(month) - 1, day)
 }
 export function isCompany(kt: string): boolean {
-  const stringKt = cleanAndValidate(kt);
-  const day = Number(stringKt.substring(0, 2));
-  return day > 31;
+  const stringKt = cleanAndValidate(kt)
+  const day = Number(stringKt.substring(0, 2))
+  return day > 31
 }
 export function getAge(kt: string | number, referenceDate?: Date): number {
-  referenceDate = referenceDate || new Date();
-  const ktDate = getBirthdate(kt);
-  const refYear = referenceDate.getFullYear();
-  const calcDate = new Date(ktDate);
-  calcDate.setFullYear(refYear);
-  let age = refYear - ktDate.getFullYear();
+  referenceDate = referenceDate || new Date()
+  const ktDate = getBirthdate(kt)
+  const refYear = referenceDate.getFullYear()
+  const calcDate = new Date(ktDate)
+  calcDate.setFullYear(refYear)
+  let age = refYear - ktDate.getFullYear()
   if (calcDate > referenceDate) {
-    age--;
+    age--
   }
   if (age < 0) {
     // þjóðskrá some times registers kennitölur with temporary last digits... add 100 to correct negative age outcome
-    return age + 100;
+    return age + 100
   }
   return age < 1
     ? (referenceDate.getTime() - ktDate.getTime()) /
@@ -167,15 +167,15 @@ export function getAge(kt: string | number, referenceDate?: Date): number {
         60 /
         24 /
         365.2422
-    : age;
+    : age
 }
 export function isKennitalaPart(str?: string): boolean {
   if (!str) {
-    return false;
+    return false
   }
   const s = str
-    .replace(" ", "")
-    .replace("-", "")
-    .trim();
-  return !isNaN(parseInt(s, 10));
+    .replace(' ', '')
+    .replace('-', '')
+    .trim()
+  return !isNaN(parseInt(s, 10))
 }
