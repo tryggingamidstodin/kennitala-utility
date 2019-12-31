a0b0f50eea81958e9ffc8691c5b461a873054f32
olafurtorfi <olafurtorfi@gmail.com>
2019-12-31 10:39:39 +0000

    replace tslint with eslint, and make compatible with prettier

---
+++ eslint.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+    env: {
+        browser: true,
+        commonjs: true,
+        es6: true,
+        node: true,
+        mocha: true,
+        jquery: true
+    },
+    extends: 'standard',
+    globals: {
+        angular: false,
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly',
+        cy: true
+    },
+    parserOptions: {
+        ecmaVersion: 2018
+    },
+    rules: {
+        'space-before-function-paren': 0,
+        indent: 'off',
+        'no-var': 'warn',
+        'no-unused-expressions': 'warn',
+        'comma-dangle': 'off'
+    },
+    ignorePatterns: ['dist/', 'node_modules/']
+}
